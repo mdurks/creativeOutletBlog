99136a43c1dc7c0d500edb94ea7fb335b607f43b
mdurks <mdurks@gmail.com>
2024-09-25 23:15:02 +0100

    Abstract away techIconMappings to own file

---
+++ src/templates/techIconMapping.js
@@ -0,0 +1,87 @@
+export const techIconMapping = {
+  axios: {
+    label: "axios",
+    cssClass: "yellow",
+    website: "https://axios-http.com/",
+  },
+  d3: {
+    label: "d3",
+    cssClass: "blue",
+    website: "https://d3js.org/",
+  },
+  framermotion: {
+    label: "framer motion",
+    cssClass: "pink",
+    website: "https://www.framer.com/motion/",
+  },
+  gatsby: {
+    label: "gatsby",
+    cssClass: "yellow",
+    website: "https://www.gatsbyjs.com/",
+  },
+  graphql: {
+    label: "graphql",
+    cssClass: "tealLight",
+    website: "https://graphql.org/",
+  },
+  gsap: {
+    label: "GreenSock animation platform",
+    cssClass: "tanzaniteDark",
+    website: "https://gsap.com/",
+  },
+  hygraph: {
+    label: "hygraph",
+    cssClass: "tanzaniteDark",
+    website: "https://hygraph.com/",
+  },
+  netlify: {
+    label: "netlify",
+    cssClass: "pinkPaleLight",
+    website: "https://www.netlify.com/",
+  },
+  r3f: {
+    label: "React three fiber",
+    cssClass: "tealDark",
+    website: "https://r3f.docs.pmnd.rs/getting-started/introduction",
+  },
+  react: {
+    label: "react",
+    cssClass: "blueBlack",
+    website: "https://react.dev/",
+  },
+  reactrouter: {
+    label: "React router",
+    cssClass: "greenDark",
+    website: "https://reactrouter.com/",
+  },
+  reduxtoolkit: {
+    label: "Redux Toolkit",
+    cssClass: "tealLight",
+    website: "https://redux-toolkit.js.org/",
+  },
+  styledcomponents: {
+    label: "styled components",
+    cssClass: "greyLight",
+    website: "https://styled-components.com/",
+  },
+  threejs: {
+    label: "three.js",
+    cssClass: "greyLight",
+    website: "https://threejs.org/",
+  },
+  vite: {
+    label: "vite",
+    cssClass: "pink",
+    website: "https://vitejs.dev/",
+  },
+  webxr: {
+    label: "Web XR",
+    cssClass: "pinkPaleLight",
+    website: "https://immersiveweb.dev/",
+  },
+  zustand: {
+    label: "zustand",
+    cssClass: "yellow",
+    website: "https://zustand.docs.pmnd.rs/getting-started/introduction",
+  },
+}
