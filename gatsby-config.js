451a335db133d514b3df26062c3e54bb765cb950
mdurks <mdurks@gmail.com>
2024-11-05 23:51:53 +0000

    Disable Google Analytics to try and fix Firefox iframe errors when loading from the Treehouse

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     title: `Creative Outlet Blog`,
     description: `Michael Durkins blog for all things web, 3D and general tech related topics.`,
     author: `mdurks@gmail.com`,
-    siteUrl: `https://creativeoutletblog.netlify.app//`,
+    siteUrl: `https://blog.michael-durkin.com/`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -59,37 +59,37 @@ module.exports = {
         icon: `src/images/creativeOutletSun.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-FNHX241BSY", // Google Analytics / GA
-          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          // origin: "YOUR_SELF_HOSTED_ORIGIN",
-          // Delays processing pageview events on route update (in milliseconds)
-          delayOnRouteUpdate: 0,
-        },
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-google-gtag`,
+    //   options: {
+    //     // You can add multiple tracking ids and a pageview event will be fired for all of them.
+    //     trackingIds: [
+    //       "G-FNHX241BSY", // Google Analytics / GA
+    //       // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+    //       // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+    //     ],
+    //     // This object gets passed directly to the gtag config command
+    //     // This config will be shared across all trackingIds
+    //     gtagConfig: {
+    //       optimize_id: "OPT_CONTAINER_ID",
+    //       anonymize_ip: true,
+    //       cookie_expires: 0,
+    //     },
+    //     // This object is used for configuration specific to this plugin
+    //     pluginConfig: {
+    //       // Puts tracking script in the head instead of the body
+    //       head: false,
+    //       // Setting this parameter is also optional
+    //       respectDNT: true,
+    //       // Avoids sending pageview hits from custom paths
+    //       exclude: ["/preview/**", "/do-not-track/me/too/"],
+    //       // Defaults to https://www.googletagmanager.com
+    //       // origin: "YOUR_SELF_HOSTED_ORIGIN",
+    //       // Delays processing pageview events on route update (in milliseconds)
+    //       delayOnRouteUpdate: 0,
+    //     },
+    //   },
+    // },
     // {
     //   resolve: "gatsby-plugin-web-font-loader",
     //   options: {
